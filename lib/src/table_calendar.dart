ff66934e7bf2bf22ee10113d039e7b69d8f15ea5
Ensar KURT <main@ensarkurt.com.tr>
2023-07-09 21:22:26 +0300

    today button added

--- lib/src/table_calendar.dart
+++ lib/src/table_calendar.dart
@@ -445,6 +445,24 @@ class _TableCalendarState<T> extends State<TableCalendar<T>> {
     );
   }
 
+  void _jumpToPage(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: widget.pageAnimationDuration,
+      curve: widget.pageAnimationCurve,
+    );
+  }
+
+  void _onTodayButtonTap() {
+    _jumpToPage(_pageController.page!.toInt() + differenceInMounth);
+
+    widget.onDaySelected?.call(_currentDay, _focusedDay.value);
+  }
+
+  int get differenceInMounth => _currentDay.month - _focusedDay.value.month;
+
+  DateTime get _currentDay => DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -462,6 +480,7 @@ class _TableCalendarState<T> extends State<TableCalendar<T>> {
                 onHeaderLongPress: () =>
                     widget.onHeaderLongPressed?.call(value),
                 headerStyle: widget.headerStyle,
+                onTodayButtonTap: _onTodayButtonTap,
                 availableCalendarFormats: widget.availableCalendarFormats,
                 calendarFormat: widget.calendarFormat,
                 locale: widget.locale,
